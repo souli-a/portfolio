7635be922a71f59ee31c854115f8be6d489059d2
souli-a <soulimane.a@protonmail.com>
2023-06-30 17:50:37 +0200

    feat: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Portfolio from './pages/Portfolio';
 import './styles/index.scss';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Portfolio />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Portfolio />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
