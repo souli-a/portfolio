f68a455f93d489b5545711e5577642e7c4369e72
souli-a <soulimane.a@protonmail.com>
2023-07-01 20:21:27 +0200

    style(mouse): disable mouse followed on mobile devices

--- src/components/Mouse.jsx
+++ src/components/Mouse.jsx
@@ -3,22 +3,37 @@ import '../styles/Mouse.scss';
 
 const Mouse = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const html = document.querySelector('html');
+    if (viewportWidth < 600) {
+      html.style.cursor = 'initial';
+    } else {
+      html.style.cursor = 'none';
+      const handleMouseMove = (e) => {
+        const { clientX, clientY } = e;
+        setMousePosition({ x: clientX, y: clientY });
+      };
+      document.addEventListener('mousemove', handleMouseMove);
+      return () => {
+        document.removeEventListener('mousemove', handleMouseMove);
+      };
+    }
+  }, [viewportWidth]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const x = mousePosition.x - 38;
   const y = mousePosition.y - 38;
-
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      const { clientX, clientY } = e;
-      setMousePosition({ x: clientX, y: clientY });
-    };
-
-    document.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
 
   return (
     <div
