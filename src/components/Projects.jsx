8483c66bfc82514c7cb5eb947e215a144329094c
souli-a <soulimane.a@protonmail.com>
2023-07-03 14:46:28 +0200

    style(ui): modify scrollbar style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -74,6 +74,36 @@ const Projects = () => {
           </div>
         </div>
       </div>
+      <div
+        className="card"
+        onClick={() => openLink('https://github.com/souli-a/portfolio')}
+      >
+        <div className="left-side-card">
+          <img src={portfolio} />
+        </div>
+        <div className="right-side-card">
+          <div className="title-card">
+            <h1 className="title-h1-card">Portfolio</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15">
+              <path d="M8.293 2.293a1 1 0 0 1 1.414 0l4.5 4.5a1 1 0 0 1 0 1.414l-4.5 4.5a1 1 0 0 1-1.414-1.414L11 8.5H1.5a1 1 0 0 1 0-2H11L8.293 3.707a1 1 0 0 1 0-1.414Z" />
+            </svg>
+          </div>
+          <div className="description-card">
+            Site personnel présentant mes projets, les technologies que
+            j'utilise et une page pour me contacter. Portfolio réalisé avec
+            React et Sass.
+          </div>
+          <div className="tag-card-list">
+            <div className="tag">React</div>
+            <div className="tag">JavaScript</div>
+            <div className="tag">Sass</div>
+            <div className="tag">Figma</div>
+            <div className="tag">Git</div>
+            <div className="tag">GitHub</div>
+            <div className="tag">Vite</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
